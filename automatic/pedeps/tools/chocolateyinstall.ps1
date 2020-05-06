af4f91546565dabd43127b4940e773b8ed123c1c
Unknown <thechaoscoder@gmail.com>
2020-05-06 21:09:35 +0200

    Update pedeps nuspec file

--- automatic/pedeps/tools/chocolateyinstall.ps1
+++ automatic/pedeps/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://github.com/brechtsanders/pedeps/releases/download/0.1.7/pedeps-0.1.7-win32.zip'
-$url64      = 'https://github.com/brechtsanders/pedeps/releases/download/0.1.7/pedeps-0.1.7-win64.zip'
-$checksum32  = '503d172b69cef6af59133ba9bd2f1e04327b542e4497098ee3d86f5b36129970'
-$checksum64  = 'ff04278bd6207c8b5c70dc9db780cac2102bf2ac34b8c76c6e9c01d53f0ebcb5'
+$url32      = 'https://github.com/brechtsanders/pedeps/releases/download/0.1.9/pedeps-0.1.9-win32.zip'
+$url64      = 'https://github.com/brechtsanders/pedeps/releases/download/0.1.9/pedeps-0.1.9-win64.zip'
+$checksum32  = 'f23517e580cd9f33bd309f9955c17d1140615d6123fdfbc67eac667da1a27c68'
+$checksum64  = '63dcb21f20bb9ea87fc5a1a935ca4fd53f8f268eb76d5b4f86a7586f48b50908'
 
 
 
